b63d962fd0c8b0d18ee0eb5cade48b4ae53af68e
Ivan Iurii <iuriiiv302@gmail.com>
2019-04-01 11:32:55 +0300

    5 commit

---
+++ Shop_Bok/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('shop/all/bok/', views.bok_detail, name='bok_detail'),
+    path('shop/all/autor/', views.autor_detail, name='autor_detail'),
+    path('shop/all/shop/', views.shop_detail, name='shop_detail'),
+    path('shop/all/', views.shop_all, name='shop_all'),
+  # path('shop/all/bok/<int:pk>/', views.bokinfo, name='bokinfo'),
+]
--- Shop_Bok/views.py
+++ Shop_Bok/views.py
@@ -2,23 +2,23 @@ from .models import Book,Author,Shop
 from django.shortcuts import render, get_object_or_404
 
 def shop_all(request):
-    return render(request, 'shop_Book/ShopBook.html', {})
+    return render(request, 'Shop_Bok/ShopBook.html', {})
 
 def bok_detail(request):
     book = Book.objects.all()
-    return render(request, "shop_Book/bok.html", {"bok": book})
+    return render(request, "Shop_Bok/bok.html", {"bok": book})
 
 def autor_detail(request):
     autor = Author.objects.all()
-    return render(request, 'shop_Book/autor.html', {"autor": autor})
+    return render(request, 'Shop_Bok/autor.html', {"autor": autor})
 
 def shop_detail(request):
    shop = Shop.objects.all()
-   return render(request, 'shop_Book/shop.html', {'shop': shop})
+   return render(request, 'Shop_Bok/shop.html', {'shop': shop})
 
 
 #def bokinfo(request, pk):
 #    idBOK = get_object_or_404(Book, pk=pk)
 #    shops = idBOK.shoop_set.all()
 #    print(shops)
-#    return render(request, 'shop_Book/infoBok.html', {'idBOK': idBOK, "shops": shops})
+#    return render(request, 'Shop_Bok/infoBok.html', {'idBOK': idBOK, "shops": shops})
