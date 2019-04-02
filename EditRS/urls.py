c1b2fac3c73d7111f07dcfc2a0d819b65b98e61a
Ivan Iurii <iuriiiv302@gmail.com>
2019-04-02 10:02:36 +0300

    12/1
    commit

--- EditRS/urls.py
+++ EditRS/urls.py
@@ -1,2 +1,21 @@
 from django.urls import path
 from . import views
+from django.conf.urls import patterns, url, include
+from rest_framework.urlpatterns import format_suffix_patterns
+from EditRS.views import UserList, UserDetail, GroupList, GroupDetail
+
+urlpatterns = patterns(
+    path('users/', views.UserList, name='user-list'),
+    path('users/<int:pk>/', views.UserDetail, name='user-detail'),
+    path('groups/', views.GroupList, name='group-list'),
+    path('groups/<int:pk>/', views.GroupDetail, name = 'group-detail')
+
+)
+
+# Format suffixes
+urlpatterns = format_suffix_patterns(urlpatterns, allowed=['json', 'api'])
+
+# Default login/logout views
+urlpatterns += patterns('',
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework'))
+)
--- EditRS/views.py
+++ EditRS/views.py
@@ -1,2 +1,48 @@
-from django.conf.urls import url, include
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
+from rest_framework import generics
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
+from rest_framework.response import Response
+from EditRS.serializers import UserSerializer, GroupSerializer
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    """
+    The entry endpoint of our API.
+    """
+    return Response({
+        'users': reverse('user-list', request=request),
+        'groups': reverse('group-list', request=request),
+    })
+
+@api_view(['GET'])
+def api_root(request, format=None):
+    """
+    The entry endpoint of our API.
+    """
+    return Response({
+        'users': reverse('user-list', request=request),
+        'groups': reverse('group-list', request=request),
+    })
+
+class UserDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    API endpoint that represents a single user.
+    """
+    model = User
+    serializer_class = UserSerializer
+
+class GroupList(generics.ListCreateAPIView):
+    """
+    API endpoint that represents a list of groups.
+    """
+    model = Group
+    serializer_class = GroupSerializer
+
+
+class GroupDetail(generics.RetrieveUpdateDestroyAPIView):
+    """
+    API endpoint that represents a single group.
+    """
+    model = Group
+    serializer_class = GroupSerializer
